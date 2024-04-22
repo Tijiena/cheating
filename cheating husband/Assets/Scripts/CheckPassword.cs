9c6f8bb001a0b433daafcde4c9419c6f170da6fe
Tiff ☆彡 <pengt796@newschool.edu>
2024-04-21 22:13:00 -0400

    password scene

---
+++ cheating husband/Assets/Scripts/CheckPassword.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class CheckPassword : MonoBehaviour
+{
+    public TMP_InputField passwordPanel;
+    public string myPassword;
+    public GameObject loadingScreen;
+
+
+    public void CheckInput()
+    {
+        if (passwordPanel.text == myPassword)
+        {
+            //SceneManager.LoadScene("main_scene");
+            loadingScreen.SetActive(true);
+            Invoke("NextLevel", 5f);
+        }
+        else
+        {
+            Debug.Log("incorrect");
+        }
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene("main_scene");
+    }
+}
---
+++ cheating husband/Assets/Scripts/HintButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintButton : MonoBehaviour
+{
+    public GameObject hintText;
+
+    public void ButtonPressed()
+    {
+        hintText.SetActive(true);
+    }
+}
